d7499055dd824f1d5e5b1f2c64a2755c39bebe46
salluzzi.luca <salluzzi.luca@gmail.com>
2021-04-22 15:21:57 -0300

    first push

---
+++ malasuertecharly.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  char letra_charly;
+  int numero_charly;
+
+  printf("Charly tirate esa letra pa: ");
+  scanf("%c", &letra_charly);
+
+  printf("Charly tirate ese numero pa: ");
+  scanf("%i", &numero_charly );
+  printf("la letra que eleigio Charly es: %c y el numero que eligio Charly es %i", letra_charly);
+  return 0;
+}
