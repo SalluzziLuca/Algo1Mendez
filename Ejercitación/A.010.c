3c7de370492729974f11f8c4771b039a324cfa36
salluzziluca <salluzzi.luca@gmail.com>
2021-05-02 02:33:52 -0300

    dale vieja por favor te pido funciona

--- Ejercitación/A.010.c
+++ Ejercitación/A.010.c
@@ -3,7 +3,7 @@
 int main(){
 	int cantidad_repetidas;
 	scanf("%i", &cantidad_repetidas);
-	// Aqui va tu codigo donde deberias usar un for..
+	// Aqui va tu codigo donde deberias usar un for..PERO LA CONCHA DE MI AMDRE
 
 	for (int cantidades_totales = 1; cantidades_totales < cantidad_repetidas; cantidades_totales++) {
 		printf("Brinco, brinco brinco, y allá voy yo.");
