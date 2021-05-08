ecabf313df9a9a826319ee8e40b5a8896cece1d7
salluzziluca <salluzzi.luca@gmail.com>
2021-05-08 20:30:43 -0300

    Cree la funcion <encuesta_television>

--- TPs/TP_1.c
+++ TPs/TP_1.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-const 
+const char POLAR = 'I';
+const char PANDA = 'P';
+const char PARDO = 'G';
+const char ANIME = 'A';
+const char MUSICA_POP = 'M';
+const char LIMPIEZA = 'L';
+
+bool es_canal_valido (char eleccion_television){
+      return (eleccion_television != ANIME  && eleccion_television != MUSICA_POP && eleccion_television != LIMPIEZA);
+}
+
+
+void encuesta_television(char* eleccion_television){
+  printf("Vas a ver televisión un rato, pones el canal de: Anime (%c), Musica Pop (%c), Limpieza (%c).\n", ANIME, MUSICA_POP, LIMPIEZA );
+  scanf(" %c", eleccion_television);
+
+  while (es_canal_valido(*eleccion_television)){
+		printf("Letra no valida, ingresar Anime (%c), Musica Pop (%c) o Limpieza (%c).\n", ANIME, MUSICA_POP, LIMPIEZA);
+		scanf(" %c", eleccion_television);
+	}
+}
+
+
+
+int main() {
+  char eleccion_television;
+  encuesta_television(&eleccion_television);
+
+  return 0;
+}
