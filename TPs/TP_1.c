7f7c00784c23d4d9a7be18c803e3a1d5bb9476c7
salluzziluca <salluzzi.luca@gmail.com>
2021-05-08 21:10:20 -0300

    Cree la funcion <encuesta_alimento> y su respectivo booleano <es_alimento_valido>

--- TPs/TP_1.c
+++ TPs/TP_1.c
@@ -7,6 +7,9 @@ const char PARDO = 'G';
 const char ANIME = 'A';
 const char MUSICA_POP = 'M';
 const char LIMPIEZA = 'L';
+const char BAMBU = 'B';
+const char PESCADO = 'P';
+const char FOCAS = 'F';
 
 bool es_canal_valido (char eleccion_television){
       return (eleccion_television != ANIME  && eleccion_television != MUSICA_POP && eleccion_television != LIMPIEZA);
@@ -23,11 +26,31 @@ void encuesta_television(char* eleccion_television){
 	}
 }
 
+bool es_alimento_valido (char eleccion_alimento){
+  return (eleccion_alimento != BAMBU  && eleccion_alimento != PESCADO && eleccion_alimento != FOCAS);
+}
+void encuesta_alimento(char* eleccion_alimento){
+  printf("Solo podes guardar un alimento en tu vianda: Bambú (%c), Pescado (%c), Focas (%c).\n", BAMBU, PESCADO, FOCAS);
+  scanf(" %c", eleccion_alimento);
+
+  while (es_alimento_valido(*eleccion_alimento)) {
+    printf("Letra no valida, ingresar Bambu (%c), Pescado (%c) o Focas (%c).\n", BAMBU, PESCADO, FOCAS);
+		scanf(" %c", eleccion_alimento);
+  }
+
+void encuesta_piso(int* eleccion_piso){
+    printf("e compras una torre con tus dos hermanos de 18 pisos. ¿En que piso te gustaría vivir?\n", BAMBU, PESCADO, FOCAS);
+    scanf(" %c", eleccion_alimento);
+
+}
 
 
 int main() {
   char eleccion_television;
+  char eleccion_alimento;
   encuesta_television(&eleccion_television);
+  encuesta_alimento(&eleccion_alimento);
+
 
   return 0;
 }
