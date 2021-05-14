5453f05c6e5e4b1856269e46b7ad6e5400e5d9a9
salluzzi.luca <salluzzi.luca@gmail.com>
2021-05-14 15:52:49 -0300

    le saqué el IF que iba de más

--- TPs/test_de_personalidad.c
+++ TPs/test_de_personalidad.c
@@ -140,13 +140,8 @@ int pasaje_de_alimento_a_numero (char eleccion_alimento){
    Post: Va a retorar, despues de hacer el calculo,un int entre 5 y 63 (inclusive) */
 int formula_puntaje_total(int valor_numerico_comida, int valor_numerico_canal, int eleccion_piso , int eleccion_grito){
 
-  if ((valor_numerico_comida == VALOR_BAMBU || valor_numerico_comida == VALOR_FOCAS || valor_numerico_comida == VALOR_PESCADO) && (valor_numerico_canal == VALOR_CANAL_ANIME || valor_numerico_canal == VALOR_CANAL_LIMPIEZA == || valor_numerico_canal == VALOR_CANAL_MUSICA_POP) && (eleccion_piso >= PISO_MINIMO && eleccion_piso <= PISO_MAXIMO) && (eleccion_grito >= GRITO_MINIMO && eleccion_grito <= GRITO_MAXIMO)) {
-
     return (valor_numerico_comida * valor_numerico_canal) + eleccion_piso + eleccion_grito;
-  }
-  else{
-    return 0;
-  }
+
 }
 
 /* Pre: Que puntaje_total sea un int 5 y 63 (inclusive).
@@ -163,7 +158,7 @@ void determinacion_del_personaje (int puntaje_total){
   else if (puntaje_total >= PUNTAJE_MINIMO_PARDO && puntaje_total <= PUNTAJE_MAXIMO_PARDO) {
     printf("Tu personalidad es de oso - Pardo (%c) -.", PARDO);
     }
-    
+
   else{
     printf("me parece que vos y yo vamo a tener un problema\n");
     }
