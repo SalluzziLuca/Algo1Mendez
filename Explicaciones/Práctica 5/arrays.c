6a8c41008f90025d664ca7eb805677c641c998e0
salluzzi.luca <salluzzi.luca@gmail.com>
2021-05-25 20:09:04 -0300

    Explicacion de arrays modularizada, agregado printf

--- Explicaciones/Práctica 5/arrays.c
+++ Explicaciones/Práctica 5/arrays.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-const int MAXIMO_VALOR;
-int main() {
-  int mi_array[10];
-
+#define MAXIMO_VALOR 10
+/* Cuando i es igual a 0, y mientras i sea menor que 10, le asignamos a cada valor del array "0" y sumamos i++ */
+void modificar_valor_array(int un_arreglo[]){
   int i;
-   for(i=0;i<=MAXIMO_NUMERO;i++){
-     mi_array[i]=0;
+   for(i=0;i<MAXIMO_VALOR;i++){
+     un_arreglo[i]=0;
    }
+}
+
+void mostrar_arreglo(int un_arreglo[]){
+  int i;
+   for(i=0;i<MAXIMO_VALOR;i++){
+     printf("%s",un_arreglo[i] );
+   }
+}
+
+int main() {
+  int mi_array[MAXIMO_VALOR];
+
+  modificar_valor_array(mi_array);
+  mostrar_arreglo(mi_array);
+
   return 0;
 }
