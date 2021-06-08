aa5eebf41d2cff0c1f96041adc4bc45ed149d244
salluzzi.luca <salluzzi.luca@gmail.com>
2021-06-08 10:20:29 -0300

    ARREGLAO

--- Explicaciones/teorica_7/recursividada.c
+++ Explicaciones/teorica_7/recursividada.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 long factorial(int un_numero){
-  if (un_numero == 0)
+  if (un_numero == 0) //caso base!!!!
   return 1;
   else
   return un_numero * factorial(un_numero-1);
 }
  int main() {
   int numero = 0;
-  scanf("%d", numero);
+  scanf("%d", &numero);
 
    printf("%li", factorial(numero));
    return 0;
