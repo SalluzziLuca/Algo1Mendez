7df22c3f5e14216633a40e22eaea68699199c470
salluzziluca <salluzzi.luca@gmail.com>
2021-06-08 10:23:45 -0300

    resolvido

--- Explicaciones/teorica_7/recursividada.c
+++ Explicaciones/teorica_7/recursividada.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-long factorial(int un_numero){
+long factorial(unsigned int un_numero){ //unisgned porque solo toma numeros positivos
   if (un_numero == 0) //caso base!!!!
   return 1;
   else
-  return un_numero * factorial(un_numero-1);
+  return un_numero * factorial(un_numero-1); //la fucnion usa a la misma funcion hasta que un_numero== 0 y se corta
 }
  int main() {
   int numero = 0;
   scanf("%d", &numero);
 
-   printf("%li", factorial(numero));
+   printf("%li \n", factorial(numero));
    return 0;
  }
